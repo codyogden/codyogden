819fa66737ba491b579c3fccb4268f469c08c479
Cody Ogden <cody@codyogden.com>
2021-09-30 16:46:51 -0500

    Add analytics.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,16 @@ class MyDocument extends Document {
     }
 
     render() {
+
+        const analytics = () => {
+            return (<script async defer data-website-id="a4f8fe20-bd0b-4a43-887b-00659eee684e" src="https://analytics.bale.media/umami.js"></script>);
+        }
+
         return (
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    {analytics()}
                 </Head>
                 <body>
                     <Main />
