f8493172469e9fb97a423ab7804958aadb685145
Cody Ogden <cody@codyogden.com>
2022-03-05 10:21:22 -0600

    Updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
 import '../assets/styles/global.scss';
+import Script from 'next/script';
+
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <>
+        <Script
+            async
+            defer
+            data-website-id="a4f8fe20-bd0b-4a43-887b-00659eee684e"
+            src="https://analytics.bale.media/umami.js"
+        />
+        <Component {...pageProps} />
+    </>
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,16 +7,10 @@ class MyDocument extends Document {
     }
 
     render() {
-
-        const analytics = () => {
-            return (<script async defer data-website-id="37160455-930a-438f-8d18-730be9f76f29" src="https://analytics.bale.media/umami.js"></script>);
-        }
-
         return (
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    {analytics()}
                 </Head>
                 <body>
                     <Main />
