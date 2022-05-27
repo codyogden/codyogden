067a8c60361ca7f3e0e80a63f13d78d0e562f066
Cody Ogden <cody@codyogden.com>
2022-05-27 12:51:55 -0500

    revert(wp): featured image was just slow to load

--- apps/wp/wp-content/themes/codyogden/functions.php
+++ apps/wp/wp-content/themes/codyogden/functions.php
@@ -5,4 +5,4 @@ add_theme_support( 'align-wide' );
 add_theme_support( 'disable-custom-colors' );
 add_theme_support( 'disable-custom-font-sizes' );
 add_theme_support( 'editor-font-sizes', array() );
-add_theme_support( 'post-thumbnails', array( 'post' ) );
+add_theme_support( 'post-thumbnails' );
