bf736a45100a3a61228614bda1e07e2e9cb1e4dc
Cody Ogden <cody@codyogden.com>
2022-05-27 12:47:53 -0500

    fix(wp): Post thumbnails not working.

--- apps/wp/wp-content/themes/codyogden/functions.php
+++ apps/wp/wp-content/themes/codyogden/functions.php
@@ -1,9 +1,8 @@
 <?php
 
-
-add_theme_support('menus');
-add_theme_support('align-wide');
-add_theme_support('disable-custom-colors');
-add_theme_support('disable-custom-font-sizes');
-add_theme_support('editor-font-sizes', array());
-add_theme_support('post-thumbnails');
+add_theme_support( 'menus' );
+add_theme_support( 'align-wide' );
+add_theme_support( 'disable-custom-colors' );
+add_theme_support( 'disable-custom-font-sizes' );
+add_theme_support( 'editor-font-sizes', array() );
+add_theme_support( 'post-thumbnails', array( 'post' ) );
