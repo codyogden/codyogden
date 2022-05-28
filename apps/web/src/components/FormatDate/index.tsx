7302b1912ed32cff30d0f2ec59bdf75dc464715e
Cody Ogden <cody@codyogden.com>
2022-05-28 10:14:45 -0500

    fix(web/components/FormatDate): Resolve Safari error.

--- apps/web/src/components/FormatDate/index.tsx
+++ apps/web/src/components/FormatDate/index.tsx
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import { FC, HTMLAttributes, useMemo } from 'react';
 
 interface Props extends HTMLAttributes<HTMLTimeElement> {
@@ -9,8 +9,17 @@ const FormatDate: FC<Props> = ({
     dateTime,
     className,
 }) => {
-    const date = useMemo(() => format(new Date(dateTime), 'd LLLL yyyy'), [dateTime]);
-    return <time className={className} dateTime={dateTime}>{date}</time>
+    const date = useMemo(() => {
+        const date_iso = new Date(parseISO(dateTime));
+        const display = format(date_iso, 'd LLLL yyyy');
+        const dateTimeFmt = format(date_iso, 'yyyy-mm-d');
+        return {
+            date_iso,
+            display,
+            dateTimeFmt,
+        }
+    }, [dateTime]);
+    return <time className={className} dateTime={date.dateTimeFmt}>{date.display}</time>
 };
 
 export default FormatDate;
