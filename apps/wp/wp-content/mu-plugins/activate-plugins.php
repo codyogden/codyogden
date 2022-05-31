0e07b7b832f4eee0c4cac29bed7a06c62f459336
Cody Ogden <cody@codyogden.com>
2022-05-31 00:19:51 -0500

    ci(wp): Test action.

---
+++ apps/wp/wp-content/mu-plugins/activate-plugins.php
@@ -0,0 +1,7 @@
+<?php
+
+// add_action('wp', function(){
+//     activate_plugin('advanced-custom-fields-pro/acf.php');
+//     activate_plugin('headless/headless.php');
+//     activate_plugin('photos/photos.php');
+// });
