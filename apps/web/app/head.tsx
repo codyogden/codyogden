3ad023fc4db29d7bc8b5c28d8fd125fee01de77f
Cody Ogden <cody@codyogden.com>
2023-01-31 22:47:08 -0600

    Update

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title>Hello, Next.js!</title>
+            <title>Hello, World!</title>
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1"
