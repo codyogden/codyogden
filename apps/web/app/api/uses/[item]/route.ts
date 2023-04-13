7a5c8163984c31e3c3064dec5660fb93fe7f8dad
Cody Ogden <cody@codyogden.com>
2023-04-13 00:59:47 -0500

    debug vercel

--- apps/web/app/api/uses/[item]/route.ts
+++ apps/web/app/api/uses/[item]/route.ts
@@ -1,5 +1,7 @@
 export async function GET(request: Request, { params }) {
-    const { data } = await (await fetch(`https://${process.env.VERCEL_URL}/api/uses`)).json();
+    const url = new URL(request.url);
+    const { data } = await (await fetch(`${url.protocol}//${url.hostname}${(url.port === '3000') ? `:${url.port}` : ''}/api/uses`)).json();
     // @ts-ignore: Returning Response.json runtime error
     return Response.json(data.filter(({ slug }) => slug === params.item)[0] ?? { notFound: true });
+
 }
--- apps/web/app/uses/[item]/page.tsx
+++ apps/web/app/uses/[item]/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 
 const Page = async ({ params }) => {
     // console.log('API URL', `https://${process.env.VERCEL_URL}/api/uses/${params.item}`);
-    const item = await (await fetch(`https://${process.env.VERCEL_URL}/api/uses/${params.item}`)).json();
+    const url = new URL(`/api/uses/${params.item}`, process.env.VERCEL_URL);
+    console.log('url', url);
+    const item = await (await fetch(url)).json();
     
-    return (item) ? <>
+    return <>
         <Image src={item.image.src} alt={item.image.alt ?? `${item.name} by ${item.company.name}`} width={item.image.width} height={item.image.height} />
-    </>: <>not working</>;
+    </>;
 };
 
 export default Page;
