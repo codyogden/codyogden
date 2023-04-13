e304751dd9191c4be83f7153e9cd9d09ed972e7a
Cody Ogden <cody@codyogden.com>
2023-04-13 11:58:23 -0500

    debug vercel

--- apps/web/app/uses/[item]/page.tsx
+++ apps/web/app/uses/[item]/page.tsx
@@ -1,12 +1,5 @@
-import Image from 'next/image';
-
-const Page = async ({ params }) => {
-    // console.log('API URL', `https://${process.env.VERCEL_URL}/api/uses/${params.item}`);
-    // console.log(await (await fetch(`htt/ps://codyogden-git-next-codyogden.vercel.app/api/uses/${params.item}`)).json());
-    
-    return <>hello
-        {/* <Image src={item.image.src} alt={item.image.alt ?? `${item.name} by ${item.company.name}`} width={item.image.width} height={item.image.height} /> */}
-    </>;
+const Page = async () => {    
+    return <>hello</>;
 };
 
 export default Page;
