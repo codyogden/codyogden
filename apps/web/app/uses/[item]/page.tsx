ba65094bb185bdfd94e101eb99631c25d69a43e9
Cody Ogden <cody@codyogden.com>
2023-04-13 00:44:14 -0500

    debug vercel

--- apps/web/app/uses/[item]/page.tsx
+++ apps/web/app/uses/[item]/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 
 const Page = async ({ params }) => {
-    console.log('API URL', `https://${process.env.VERCEL_URL}/api/uses/${params.item}`);
+    // console.log('API URL', `https://${process.env.VERCEL_URL}/api/uses/${params.item}`);
     const item = await (await fetch(`https://${process.env.VERCEL_URL}/api/uses/${params.item}`)).json();
+    console.log(item);
     return <>
         <Image src={item.image.src} alt={item.image.alt ?? `${item.name} by ${item.company.name}`} width={item.image.width} height={item.image.height} />
     </>;
