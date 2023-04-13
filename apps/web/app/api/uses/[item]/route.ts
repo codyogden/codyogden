2af9fb1cb1ca9bbd89abb0ebbf8e57673c4ee282
Cody Ogden <cody@codyogden.com>
2023-04-13 00:30:31 -0500

    debug vercel

--- apps/web/app/api/uses/[item]/route.ts
+++ apps/web/app/api/uses/[item]/route.ts
@@ -1,5 +1,5 @@
-export async function GET(request: Request) {
+export async function GET(request: Request, { params }) {
     const { data } = await (await fetch(`https://${process.env.VERCEL_URL}/api/uses`)).json();
     // @ts-ignore: Returning Response.json runtime error
-    return Response.json(data.filter(({ slug }) => slug === new URL(request.url).searchParams.get('item'))[0] ?? { notFound: true });
+    return Response.json(data.filter(({ slug }) => slug === params.item)[0] ?? { notFound: true });
 }
