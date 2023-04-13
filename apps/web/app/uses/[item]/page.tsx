899138e9185fee04f4986e7e0aeacbe9abcd2b3a
Cody Ogden <cody@codyogden.com>
2023-04-13 12:29:24 -0500

    debug vercel

--- apps/web/app/uses/[item]/page.tsx
+++ apps/web/app/uses/[item]/page.tsx
@@ -1,9 +1,12 @@
-interface PageParams {
-    params: {
-        slug: string;
-    }
-}
+import Image from 'next/image';
 
-export default function Page({ params }: PageParams) {
-    return <h1>My Page</h1>;
-}
+const Page = async ({ params }) => {
+    // console.log('API URL', `https://${process.env.VERCEL_URL}/api/uses/${params.item}`);
+    const item = await (await fetch(`https://${process.env.VERCEL_URL}/api/uses/${params.item}`)).json();
+    
+    return <>
+        <Image src={item.image.src} alt={item.image.alt ?? `${item.name} by ${item.company.name}`} width={item.image.width} height={item.image.height} />
+    </>;
+};
+
+export default Page;
