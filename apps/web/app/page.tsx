a00e824d402b6124b0c54e7821a2010a1caabe9f
Cody Ogden <cody@codyogden.com>
2023-04-21 19:42:27 -0500

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
             </p>
             <p>I've been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework--fifth grade math is tough! I've held many hats in my career: individual contributor, technical lead, engineering manager, and company leadership. I've been lucky to work with companies like General Mills, Target, Best Buy, Rolex, and Charter Communications where I lead scalable, maintainable technology solutions that impact their business, people, and customers.</p>
             <p>You might know me as the groundskeeper at the <a href="https://killedbygoogle.com">Google graveyard</a> where I research Google's consumer product strategy and their 'killer' reputation.</p>
-            <p>My dog, Olly, and I live in Minneapolis, Minnesota.</p>
+            <p>My dog, Olly, and I live in San Francisco, California.</p>
  </div>
     </div>;
 }
