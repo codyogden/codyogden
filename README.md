76022bbd54efd6daf739cca5b652d410bc32f661
Cody Ogden <cody@codyogden.com>
2023-08-13 11:33:37 -0700

    feat: bio update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Hi! I'm Cody Ogden.
 
-I’ve been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework–fifth grade math is tough! I’ve worn many hats in my tech career: individual contributor, technical lead, engineering manager, and company leadership roles. I’m particularly interested in developer experience work, web optimization, and anything and everything involved in building, deploying, and optimizing large-scale Next.js applications. I also spend some of my spare time as the notoriously snarky cryptkeeper of [dead Google products](https://killedbygoogle.com).
+I’ve been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework—fifth grade math is tough! I've held many hats in my career from individual contributor, tech lead, engineering manager, and even company leadership. These days I’m using my experience to design teams and processes which enable, support, and impact the experience of developer customers.
 
-I live in San Francisco, California with my black goldendoodle, Olly.
+It’s no secret: developers like talking to developers when they’re solving developer problems. It takes a team filled with technical aptitude, empathy, and excellent communication skills to deliver high value support experiences and build relationships with developer customers. And that’s exactly the kind of teams I assemble and lead with a mission to ensure developers have the best experience possible with my company’s products & technology.
+
+Outside of work, you might know me as the groundskeeper of the [Google Graveyard](https://killedbygoogle.com) where I research Google’s consumer product strategy and their ‘killer’ reputation.
+
+My dog, Olly, and I live in San Francisco, California.
 
 <a rel="me nofollow" href="https://mastodon.social/@codyogden">&nbsp;</a>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -17,8 +17,9 @@ export default function Page() {
                 fontWeight: 'inherit',
             }}>Cody Ogden</h1>.</div> <p style={{ display: 'inline' }}> I like to do internet things with internet people.
             </p>
-            <p>I've been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework--fifth grade math is tough! I've held many hats in my career: individual contributor, technical lead, engineering manager, and company leadership. I've been lucky to work with companies like General Mills, Target, Best Buy, Rolex, and Charter Communications where I lead scalable, maintainable technology solutions that impact their business, people, and customers.</p>
-            <p>You might know me as the groundskeeper at the <a href="https://killedbygoogle.com">Google graveyard</a> where I research Google's consumer product strategy and their 'killer' reputation.</p>
+            <p>I’ve been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework—fifth grade math is tough! I've held many hats in my career from individual contributor, tech lead, engineering manager, and even company leadership. These days I’m using my experience to design teams and processes which enable, support, and impact the experience of developer customers.</p>
+            <p>It’s no secret: developers like talking to developers when they’re solving developer problems. It takes a team filled with technical aptitude, empathy, and excellent communication skills to deliver high value support experiences and build relationships with developer customers. And that’s exactly the kind of teams I assemble and lead with a mission to ensure developers have the best experience possible with my company’s products & technology.</p>
+            <p>Outside of work, you might know me as the groundskeeper of the <a href="https://killedbygoogle.com">Google Graveyard</a> where I research Google’s consumer product strategy and their ‘killer’ reputation.</p>
             <p>My dog, Olly, and I live in San Francisco, California.</p>
  </div>
     </div>;
