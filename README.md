ae3c531948e71755d04c83a749b7913706480cb9
Cody Ogden <cody@codyogden.com>
2023-10-13 00:00:33 -0700

    feat: update bio

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ I’ve been crafting on the web since 2001, and it all started with a JavaScript
 
 It’s no secret: developers like talking to developers when they’re solving developer problems. It takes a team filled with technical aptitude, empathy, and excellent communication skills to deliver high value support experiences and build relationships with developer customers. And that’s exactly the kind of teams I assemble and lead with a mission to ensure developers have the best experience possible with my company’s products & technology.
 
+I currently work with the incredible people building the future of the web at [Vercel](https://vercel.com).
+
 Outside of work, you might know me as the groundskeeper of the [Google Graveyard](https://killedbygoogle.com) where I research Google’s consumer product strategy and their ‘killer’ reputation.
 
 My dog, Olly, and I live in San Francisco, California.
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,6 +19,7 @@ export default function Page() {
             </p>
             <p>I’ve been crafting on the web since 2001, and it all started with a JavaScript program to help me cheat on math homework—fifth grade math is tough! I've held many hats in my career from individual contributor, tech lead, engineering manager, and even company leadership. These days I’m using my experience to design teams and processes which enable, support, and impact the experience of developer customers.</p>
             <p>It’s no secret: developers like talking to developers when they’re solving developer problems. It takes a team filled with technical aptitude, empathy, and excellent communication skills to deliver high value support experiences and build relationships with developer customers. And that’s exactly the kind of teams I assemble and lead with a mission to ensure developers have the best experience possible with my company’s products & technology.</p>
+            <p>I currently work with the incredible people building the future of the web at <a href="https://vercel.com">Vercel</a>.</p>
             <p>Outside of work, you might know me as the groundskeeper of the <a href="https://killedbygoogle.com">Google Graveyard</a> where I research Google’s consumer product strategy and their ‘killer’ reputation.</p>
             <p>My dog, Olly, and I live in San Francisco, California.</p>
  </div>
